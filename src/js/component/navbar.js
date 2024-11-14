f9ade6f24bf28cb9651e064efd663e8d3646ba43
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-11-14 20:27:20 +0000

    terminado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,17 +11,17 @@ export const Navbar = () => {
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1 p-2 text-light">Star Wars</span>
 				</Link>
-				<div className="mx-auto">
+				<div className="me-5">
 					<div className="nav-item dropdown">
 						<a className="nav-link dropdown-toggle text-light"
 							href="#" role="button"
 							data-bs-toggle="dropdown"
 							aria-expanded="false" >
 							Favoritos</a>
-						<ul className="dropdown-menu" style={{backgroundColor:"rgb(255,255,255,0.1"}} >
+						<ul className="dropdown-menu me-5" style={{backgroundColor:"rgb(255,255,255,0.1"}} >
 							{store.misFavoritos.map((item, id) => (
-								<li key={id}>
-									<a className="dropdown-item-center text-light" onClick={() => actions.favoritos(item)}>
+								<li key={id} className="">
+									<a className="dropdown-item-center text-light d-flex justify-content-around align-items-center me-2" onClick={() => actions.favoritos(item)}>
 										{item} <i class="fa fa-trash float-end"></i></a>
 								</li>))}
 						</ul>
